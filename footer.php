cbffd25cdf82a61064f51ef6530555a2cde179bb
jvinge <justin@deviine.com>
2014-11-07 14:54:59 -0800

    deleted all the wrong content files

--- footer.php
+++
@@ -1,4 +0,0 @@
-<footer><p>&copy; <?php bloginfo('name'); ?> All Rights Reserved. <?php echo date('Y'); ?></p><br>
-<span>Must be 21 years or older and live in a state that has legalized recreational cannabis.</span>
-</footer>
-<?php wp_footer(); ?>
