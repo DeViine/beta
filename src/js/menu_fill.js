bddb4806a5a63de2a6f5b66a01cb0363db5ffe3f
jvinge <justin@deviine.com>
2015-02-10 17:04:37 -0800

    updates to the new menu to firebase script

--- src/js/menu_fill.js
+++ src/js/menu_fill.js
@@ -7,22 +7,26 @@
 	//order the menu items alphabetically according to itemname from the data set
 	//create validations to be sure the form is complete
 	//create a function to replace data if data exists. So firebase set rather than firebase push or visa versa
-$('menu_submit').submit(menuItemSet(complete){
-	complete.preventDefault();
-		/* Insert all validations here as if else checks */
-	
-	/* IF the value of option is new THEN invoke function 'firebasePush' ELSE invoke function 'firebaseSet' */
-	if $(this).value('new') {
-		
-	}
-	/* If a dispensary already exists in the form dropdown, then run the firebase set function to replace old data */
-	/* If a dispensary does not already exist in the form dropdown, then run the firebase push function to insert new data */
-	$this.submit( firbasePush() {
-		/* Push all of the form field inputs to firebase for INSERTING NEW CONTENT */
-	});
+(function() {
+	var menuRef = new Firebase('https://dazzling-inferno-1178.firebaseIO.com/menu');  // Generate a Firebase location
+	$('#menu_submit').submit(function menuItemSet(complete){
+			//complete.preventDefault();
+				/* Insert all validations here as if else checks */
+			
+			/* IF the value of option is new THEN invoke function 'firebasePush' ELSE invoke function 'firebaseSet' */
+			if ($('option')[0].val() == "new") {
+	                                          
+				/************ Set the input of the dispensary field as the name of the new key in firebase ****************/
+				menuRef.child('dispensary').set($(this)[1].value);//Gets the value of the first text field and sets it to firebase under dispensary
+			}
+			/* If a dispensary already exists in the form dropdown, then run the firebase set function to replace old data */
+			/* If a dispensary does not already exist in the form dropdown, then run the firebase push function to insert new data */
+			$(this).submit(function firbasePush() {
+				/* Push all of the form field inputs to firebase for INSERTING NEW CONTENT */
+	    })
 
-	$this.submit( firbaseSet() {
-		/* Set all of the form field inputs to firebase for REPLACING OLD CONTENT */
+			$(this).submit(function firbaseSet() {
+				/* Set all of the form field inputs to firebase for REPLACING OLD CONTENT */
+			});
 	});
-
 });
