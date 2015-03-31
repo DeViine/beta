e0204faf0678d557aae8566f94567d43e9122586
jvinge <justin@deviine.com>
2015-03-31 14:05:39 -0700

    changed html5 to true in app

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ angular.module('DeViine', ['DeViine.config', 'DeViine.services', 'DeViine.contro
   .config(function($stateProvider, $urlRouterProvider, $locationProvider) {
     
     $locationProvider
-      .html5Mode(false)
+      .html5Mode(true)
       .hashPrefix('!');
     
     
