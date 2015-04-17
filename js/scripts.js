d4ba8ec6d5e3152b33c2839544fcec81d1e11452
jvinge <justin@deviine.com>
2015-04-16 17:01:47 -0700

    added the zoom funcionality and all images necessary. WARNING: zoom breaks nav if there is no Large version of the bud image avaialable. Dakota also made other adjustments

--- js/scripts.js
+++ js/scripts.js
@@ -8,45 +8,21 @@ window.onload = (function() {
     });
     // Image Zoom Ends Here
 
-    // Ratings Hover Starts Here
-    $('#rate1').on('mouseenter', function() {
-		$('#rate1').css("background-color", "rgb(44,48,52)");
-	});
-	$('#rate2').on('mouseenter', function() {
-		$('#rate1,#rate2').css("background-color", "rgb(44,48,52)");
-	});
-	$('#rate3').on('mouseenter', function() {
-		$('#rate1,#rate2,#rate3').css("background-color", "rgb(44,48,52)");
-	});
-	$('#rate4').on('mouseenter', function() {
-		$('#rate1,#rate2,#rate3,#rate4').css("background-color", "rgb(44,48,52)");
-	});
-	$('#rate5').on('mouseenter', function() {
-		$('#rate1,#rate2,#rate3,#rate4,#rate5').css("background-color", "rgb(44,48,52)");
-	});
-	$('#rate1').on('mouseleave', function() {
-		$('#rate1').css("background-color", "rgba(44,48,52,.1)");
-	});
-	$('#rate2').on('mouseleave', function() {
-		$('#rate1,#rate2').css("background-color", "rgba(44,48,52,.1)");
-	});
-	$('#rate3').on('mouseleave', function() {
-		$('#rate1,#rate2,#rate3').css("background-color", "rgba(44,48,52,.1)");
-	});
-	$('#rate4').on('mouseleave', function() {
-		$('#rate1,#rate2,#rate3,#rate4').css("background-color", "rgba(44,48,52,.1)");
-	});
-	$('#rate5').on('mouseleave', function() {
-		$('#rate1,#rate2,#rate3,#rate4,#rate5').css("background-color", "rgba(44,48,52,.1)");
-	});
-    // Ratings Hover Ends Here
-
     // Mobile Menu Drawer Open / Close Starts Here
     $("#menuMobile-toggler").click(function(){
 		$('#menuMobile').css("left","0"); 
+		$(this).css("display","none");
+		$('#close-menuMobile').css("display","inherit");
 	});
 	$("#close-menuMobile").click(function(){
-		$('#menuMobile').css("left","-50%"); 
+		$('#menuMobile').css("left","-75%");
+		$(this).css("display","none");
+		$('#menuMobile-toggler').css("display","inline");
+	});
+	$(".mobileNavLink").click(function(){
+		$('#menuMobile').css("left","-75%");
+		$("#close-menuMobile").css("display","none");
+		$('#menuMobile-toggler').css("display","inline");
 	});
 	// Mobile Menu Drawer Open / Close Ends Here
 
